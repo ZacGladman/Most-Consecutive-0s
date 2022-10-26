2709619697a5856bfe4246cafd11bd9d7e646834
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-10-26 14:25:33 +0100

    Add test cases

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,15 @@
-import sum from "./skeleton";
+import mostConsecutiveZeroes from "./skeleton";
 
-test("sum adds two numbers", () => {
-  expect(sum(4, 3)).toBe(7);
+test("Filters out consecutive non-zero values", () => {
+  expect(mostConsecutiveZeroes(100000999954200490909080000020000000000000000000000200000)).toBe(22);
+  expect(mostConsecutiveZeroes(1111011111001)).toBe(2)
+  });
+
+test("Works with only one zero", () => {
+  expect(mostConsecutiveZeroes(9876543210)).toBe(1)
 });
+
+test("Works with only one character", () => {
+  expect(mostConsecutiveZeroes(0)).toBe(1)
+});
+
